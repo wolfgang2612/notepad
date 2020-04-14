7c6777dad855227a074bcd29a2a9a6b5c2af2e4c
Ashutosh Mishra <frozenthrone.ashutosh.mishra@gmail.com>
2020-04-15 05:21:58 +0530

    Add files via upload

---
+++ backend/models/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
